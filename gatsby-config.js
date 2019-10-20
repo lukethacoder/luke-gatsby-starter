c34599a636e8fae664eff0abb91eac2425abbb8f
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2019-10-20 13:02:24 +1100

    lint-staged / husky / add gatsby-plugin-offline, gatsby-plugin-sitemap & gatsby-plugin-nprogress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,51 @@
 const path = require(`path`)
 
 module.exports = {
-	siteMetadata: {
-		title: `Gatsby Starter Luke`,
-		description: `An opinionated GatsbyJS starter using TypeScript and React String.`,
-	},
-	plugins: [
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `luke-gatsby-starter`,
-				short_name: `starter-luke`,
-				start_url: `/`,
-				background_color: `#dfdfdf`,
-				theme_color: `#BD003D`,
-				display: `minimal-ui`,
-				icon: `src/assets/images/favicon.png`,
-			},
-		},
-		{
-			resolve: `gatsby-plugin-react-svg`,
-			options: {
-				rule: {
-					include: path.resolve(__dirname, `src/assets/svg`),
-				},
-			},
-		},
-		{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_CODE,
-			},
-		},
-		`gatsby-plugin-emotion`,
-		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-typescript`,
-		`gatsby-plugin-sharp`,
-		`gatsby-transformer-sharp`,
-	],
+  siteMetadata: {
+    title: `Gatsby Starter Luke`,
+    siteUrl: `https://gatsby-starter-luke.netlify.com`,
+    description: `An opinionated GatsbyJS starter using TypeScript and React String.`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `luke-gatsby-starter`,
+        short_name: `starter-luke`,
+        start_url: `/`,
+        background_color: `#dfdfdf`,
+        theme_color: `#BD003D`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/favicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: path.resolve(__dirname, `src/assets/svg`),
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#BD003D`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_CODE,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`,
+  ],
 }
